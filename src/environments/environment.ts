d4d8b60458a0a3eff3978380e07ebdb14777626f
AdelinImbru <103764269+AdelinImbru@users.noreply.github.com>
2023-05-11 16:55:49 +0300

    update supabase key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   supabaseURL: 'https://oqphhgyulecsgphlwjqr.supabase.co',
   supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xcGhoZ3l1bGVjc2dwaGx3anFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM2MzQyMjYsImV4cCI6MTk5OTIxMDIyNn0.-PNGh_RMj-s7MnmAk4neJGzedlC9tOcDLMjtoQ8AdBA',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xcGhoZ3l1bGVjc2dwaGx3anFyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MzYzNDIyNiwiZXhwIjoxOTk5MjEwMjI2fQ.mHIxhpF40OvxqFftHogghQfI84At6Kvk5Ta1tKR6ILg',
   serviceRole:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xcGhoZ3l1bGVjc2dwaGx3anFyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MzYzNDIyNiwiZXhwIjoxOTk5MjEwMjI2fQ.mHIxhpF40OvxqFftHogghQfI84At6Kvk5Ta1tKR6ILg',
 };
