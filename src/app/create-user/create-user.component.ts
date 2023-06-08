c0a1eb6dddebf2a1c77108f1dc6ad71b2f3a8efb
AdelinImbru <103764269+AdelinImbru@users.noreply.github.com>
2023-06-08 16:49:50 +0300

    add role field in form and change authorizedUser input type to select

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { SupabaseService } from '../services/supabase.service';
+import { EmployeesService } from '../services/employees.service';
+import { Database } from '../models/supabase';
+import { isPostgressError } from '../models/utils';
+import { RoleService } from '../services/role.service';
 
 @Component({
   selector: 'app-create-user',
@@ -10,9 +14,15 @@ import { SupabaseService } from '../services/supabase.service';
 export class CreateUserComponent implements OnInit {
   createUser: FormGroup;
   hide = true;
+  selectedRole: number;
+  selectedUser: string;
+  roles: Database['public']['Tables']['Role']['Row'][];
+  authorizedUsers: Database['public']['Tables']['Employee']['Row'][];
   constructor(
     private formBuilder: FormBuilder,
-    private supabaseService: SupabaseService
+    private supabaseService: SupabaseService,
+    private employeesService: EmployeesService,
+    private roleService: RoleService
   ) {}
   ngOnInit(): void {
     this.createUser = this.formBuilder.group({
@@ -26,18 +36,31 @@ export class CreateUserComponent implements OnInit {
       department: [''],
       accessInterval: [''],
       IMEI: [''],
+      role: [''],
       createdByAuthorisedUser: [''],
     });
     this.createUser.markAllAsTouched();
+    this.roleService.getAll().then((data) => {
+      if (isPostgressError(data)) throw data;
+      this.roles = data || [];
+    });
+    this.employeesService.getAuthorizedUsers().then((data) => {
+      if (isPostgressError(data)) throw data;
+      this.authorizedUsers = data || [];
+    });
   }
 
   async Save() {
+    let employee: Database['public']['Tables']['Employee']['Row'] =
+      this.createUser.value;
     const { data, error } =
       await this.supabaseService.supabase.auth.admin.createUser({
         email: this.createUser.value.email,
         password: this.createUser.value.password,
         email_confirm: true,
       });
+    if (error) return;
+    await this.employeesService.addEmployee(employee);
     if (error) return;
     location.href = '';
   }
